a81a992af0d6991323a7771e9c47799b6080135f
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2014-01-13 14:15:35 +0100

    Initial commit

---
+++ clipboarder.pl
@@ -0,0 +1,52 @@
+# requires xsel
+
+sub on_start {
+	my ($self, $search) = @_;
+
+	warn "Good morning sunshine\n";
+
+	$self->enable(key_press => \&key_press);
+}
+
+sub key_press {
+	my ($self, $event, $keysym, $octets) = @_;
+
+#	warn "Keypress: " . $octets . " " . $keysym . "\n";
+
+  if($keysym eq 118 && $event->{state} & urxvt::Mod1Mask) {
+    # press [alt]+v]
+    warn "Pasting\n";
+    paste($self);
+
+    return 1; # ignore XEvent
+  } 
+  if($keysym eq 99 && $event->{state} & urxvt::Mod1Mask) {
+    # press [alt]+c]
+    warn "Copying\n";
+    copy($self);
+
+    return 1; # Ignore XEvent
+  }
+
+	()
+}
+
+
+sub paste {
+  my ($self) = @_;
+
+  my $content = `xsel -ob`;
+  chomp($content);
+  $self->tt_paste($content);
+
+  ()
+}
+
+sub copy {
+  my ($self) = @_;
+
+  my $content = $self->selection;
+  system('echo "' . $content . '" | xsel -ib');
+
+  ()
+}
